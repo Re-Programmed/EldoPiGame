0f24fce1735d109adb2d5518cb8dbe2b08ea87e4
Re-Programmed <79171779+Re-Programmed@users.noreply.github.com>
2026-03-03 20:29:43 -0700

    Added 10 new pies.

--- pi_data.js
+++ pi_data.js
@@ -10,5 +10,65 @@ const PI_DATA = [
         name: "Pumpkin Pi",
         fact: "The record for reciting the most number of decimal places of Pi was achieved by Rajveer Meena at VIT University, Vellore, India on 21 March 2015. He was able to recite 70,000 decimal places.",
         picture: "./assets/pumpkin_pi.png"
+    },
+    {
+        id_code: 339534,
+        name: "Mud Pi",
+        fact: "For pi, Babylonian civilization used the fraction 25/8 and the Chinese used the integer 3. By 1665, Isaac Newton calculated pi to 16 decimal places. In the early 1700s Thomas Lagney calculated 127 decimal places of pi and in the second half of the twentieth century the number of digits of pi increased from about 2000 to 500,000. In 2017 when a Swiss scientist computed more than 22 trillion digits of pi which took over a hundred days.",
+        picture: "./assets/mud_pi.png"
+    },
+    {
+        id_code: 781236,
+        name: "Integral Pi",
+        fact: "Calculus students worst fear! Did you know that Pi Day is also Albert Einstein's Birthday?",
+        picture: "./assets/integral_pi.png"
+    },
+    {
+        id_code: 380518,
+        name: "Key Lime Pi",
+        fact: "Did you know that pi is what mathematicians call a transcendental number. That means it cannot be the answer to any equation made from ordinary numbers using addition, subtraction, multiplication, division, or exponents.",
+        picture: "./assets/key_lime_pi.png"
+    },
+    {
+        id_code: 502934,
+        name: "Golden Pi",
+        fact: "Did you know that the area under the function (e^-x^2) from 0 to infinity is sqrt(pi)/2. Isn't that weird? (e^-x^2 is the form for the equation of a normal curve.)",
+        picture: "./assets/golden_pi.png"
+    },
+    {
+        id_code: 508583,
+        name: "Shepard's Pi",
+        fact: "Did you know that during World War II, Allied forces used the digits of pi to help encode secret messages shared between the United States and the United Kingdom. Since the digits never repeat in a predictable way, they worked like a natural cipher. Each digit could stand for a different letter or symbol, making the message harder to intercept or decode.",
+        picture: "./assets/shepards_pi.png"
+    },
+    {
+        id_code: 630560,
+        name: "Lemon Meringue Pi",
+        fact: "Did you know that it took Leonhard Euler to make the symbol for pi a standard part of math. Once he started using it, others followed.",
+        picture: "./assets/lemon_meringue_pi.png"
+    },
+    {
+        id_code: 401276,
+        name: "Black Bottom Banana Cream Pie With Vanilla Wafer Crust",
+        fact: "Did you know that you only need 39 digits of pi to calculate the circumference of the observable universe to within a hydrogen atom.",
+        picture: "./assets/banana_pi.png"
+    },
+    {
+        id_code: 327150,
+        name: "Cherry Pi",
+        fact: "Did you know that at the 762nd decimal place of pi there is a sequence of six 9s in a row.",
+        picture: "./assets/cherry_pi.png"
+    },
+    {
+        id_code: 261701,
+        name: "Radioactive Nuclear Pi",
+        fact: "Did you know that adding the infinite series of fractions defined as lim(1/n^2) to infinity (1/1 + 1/4 + 1/9 + 1/16 + ...) gives a result of pi^2/6?",
+        picture: "./assets/nuclear_pi.png"
+    },
+    {
+        id_code: 604188,
+        name: "Upside-Down Pi",
+        fact: "Did you know that e^(i*pi) = -1. Wow! That equation relates 4 different mathematical constants e, i, pi, and 1.",
+        picture: "./assets/upside_down_pi.png"
     }
 ]
